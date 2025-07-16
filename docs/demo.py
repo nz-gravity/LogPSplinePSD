6040a5c429749c72d088b06ac75807eafb2070ca
Avi <avi.vajpeyi@gmail.com>
2025-07-17 10:50:26 +1200

    fix: pypi readme fix and updating demo

--- docs/demo.py
+++ docs/demo.py
@@ -4,23 +4,16 @@ import scipy
 from log_psplines.datatypes import Timeseries
 from log_psplines.mcmc import run_mcmc
 from log_psplines.plotting import plot_pdgrm
+from log_psplines.example_datasets import ARData
 
 np.random.seed(0)
 
-n_samples, fs = 1024, 100
-t = np.linspace(0, (n_samples - 1) * 1 / fs, n_samples)
-noise = scipy.signal.lfilter(
-    b=[1],
-    a=[1, -2.2137, 2.9403, -2.1697, 0.9606],
-    x=np.random.randn(n_samples),
-)
-mock_pdgrm = Timeseries(t, noise).standardise().to_periodogram().highpass(5)
 
+ar4 = ARData(order=4, duration=8.0, fs=1024.0, sigma=1.0, seed=42)
 mcmc, spline_model = run_mcmc(
-    mock_pdgrm, n_knots=30, num_samples=250, num_warmup=1000, rng_key=0
+    ar4.periodogram, n_knots=30, num_samples=250, num_warmup=1000, rng_key=0
 )
-samples = mcmc.get_samples()
 
-fig, ax = plot_pdgrm(mock_pdgrm, spline_model, samples["weights"])
+fig, ax = plot_pdgrm(ar4.periodogram, spline_model, samples["weights"])
 ax.set_axis_off()
 fig.savefig("demo.png", transparent=True, bbox_inches="tight", dpi=300)

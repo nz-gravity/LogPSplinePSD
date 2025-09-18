98733ab8c71fba1e2d4acbe55c9610ed15c81e2f
Avi <avi.vajpeyi@gmail.com>
2025-09-19 11:52:28 +1200

    Add varma dataset

--- tests/test_example_datasets.py
+++ tests/test_example_datasets.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 
 from log_psplines.example_datasets.ar_data import ARData
 from log_psplines.example_datasets.lvk_data import LVKData
+from log_psplines.example_datasets.varma_data import VARMAData
 
+import os
+
+OUT = 'out_example_datasets'
 
 def test_ar(outdir):
+    outdir = f"{outdir}/{OUT}"
+    os.makedirs(outdir, exist_ok=True)
     fig, axes = plt.subplots(2, 2, figsize=(10, 8), sharex=True)
     for i, ax in enumerate(axes.flat):
         ar_data = ARData(
@@ -22,9 +28,18 @@ def test_ar(outdir):
 
 
 def test_lvk_data(outdir):
+    outdir = f"{outdir}/{OUT}"
+    os.makedirs(outdir, exist_ok=True)
     lvk_data = LVKData.download_data(
         detector="L1",
         gps_start=1126259462,
         duration=4,
     )
     lvk_data.plot_psd(fname=f"{outdir}/lvk_psd_analysis.png")
+
+
+def test_varma_data(outdir):
+    outdir = f"{outdir}/{OUT}"
+    os.makedirs(outdir, exist_ok=True)
+    varma_data = VARMAData()
+    varma_data.plot(fname=f"{outdir}/varma_data_analysis.png")

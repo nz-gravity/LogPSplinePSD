b2b9cd7ad6c3fcb65e6798de7296194869f3436e
Avi <avi.vajpeyi@gmail.com>
2025-09-24 13:08:55 +1200

    remove dead function

--- src/log_psplines/plotting/__init__.py
+++ src/log_psplines/plotting/__init__.py
@@ -1,5 +1,5 @@
-from .diagnostics import plot_diagnostics, create_fallback_plot
+from .diagnostics import plot_diagnostics
 from .pdgrm import plot_pdgrm
 from .psd_matrix import plot_psd_matrix, compute_empirical_psd
 
-__all__ = ["plot_pdgrm", "plot_diagnostics", "create_fallback_plot", "plot_psd_matrix", "compute_empirical_psd"]
+__all__ = ["plot_pdgrm", "plot_diagnostics",  "plot_psd_matrix", "compute_empirical_psd"]

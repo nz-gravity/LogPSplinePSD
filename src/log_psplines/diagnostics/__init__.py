71cdc91c39ea63833c6adcb01fbecd74d18e1d0b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2026-09-25 22:53:09 +1200

    expose diagnostics-only ArviZ adapter

--- src/log_psplines/diagnostics/__init__.py
+++ src/log_psplines/diagnostics/__init__.py
@@ -1,10 +1,15 @@
 """Minimal diagnostics helpers."""
 
+from log_psplines.diagnostics.arviz import to_arviz
 from log_psplines.diagnostics.plot_nuts import plot_energy
-from log_psplines.diagnostics.summary_tables import build_nuts_summary_table, build_vi_summary_table
+from log_psplines.diagnostics.summary_tables import (
+    build_nuts_summary_table,
+    build_vi_summary_table,
+)
 
 __all__ = [
     "build_nuts_summary_table",
     "build_vi_summary_table",
     "plot_energy",
+    "to_arviz",
 ]
